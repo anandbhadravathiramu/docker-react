5bdfac4b11511ffc7c2852ae0a84e52c01211d0a
i349061 <a.ramu@sap.com>
2023-01-03 09:31:46 +0100

    modified test in src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and SAVE and RELOADing.
+          created by local GIT.
         </p>
         <a
           className="App-link"
